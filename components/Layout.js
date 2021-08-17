b5c70d9caeac53decd5326ed99caa5105c2edd43
noonchaa <enouchiha11@gmail.com>
2021-08-18 04:13:30 +0700

    add Layout, Navbar, and SupabaseClient

--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,22 @@
 import Head from 'next/head'
+import Navbar from './Navbar'
 
 const Layout = ({children}) => {
     return(
-        <>
+        <div className='max-w-screen-2xl mx-auto'>
         <Head>
             <title>Shopedia</title>
             <meta name="description" content="Ecommerce Web App" />
             <link rel="icon" href="/favicon.ico" />
         </Head>
         <main>
+            <Navbar/>
             {children}
         </main>
         <footer>
             <p>Shopedia @2021</p>
         </footer>
-        </>
+        </div>
     )
 }
 export default Layout
---
+++ components/Navbar.js
@@ -0,0 +1,40 @@
+import { useState } from "react"
+import {CgMenuRightAlt,CgMenuRight} from 'react-icons/cg'
+
+const Navbar = () => {
+    const [open, setOpen] = useState(false)
+
+    const Menu = () => {
+        return(
+            <div className='flex justify-evenly align-middle'>
+                <h1 className='bg-green-600 rounded-md px-4 py-1 text-white text-lg font-semibold tracking-wider ml-4'>
+                    Login
+                </h1>
+                <h1 className='bg-green-600 rounded-md px-4 py-1 text-white text-lg font-semibold tracking-wider ml-4'>
+                    Register
+                </h1>
+            </div>
+        )
+    }
+    return(
+        <div className='bg-gray-200'>
+            <div className='flex justify-between align-middle px-4 py-3'>
+                <h1 className='text-4xl font-semibold text-green-500'>
+                    Shopedia
+                </h1>
+                <div className='md:hidden' onClick={()=>setOpen(!open)}>
+                    {open==false?
+                    <CgMenuRightAlt className='w-10 h-10 text-green-500'/>:
+                    <CgMenuRight className='w-10 h-10 text-green-500'/>}
+                </div>
+                <div className='hidden md:block'>
+                    <Menu/>
+                </div>
+            </div>
+            <div className={open==false?'hidden':'block md:hidden'}>
+                <Menu/>
+            </div>
+        </div>
+    )
+}
+export default Navbar
